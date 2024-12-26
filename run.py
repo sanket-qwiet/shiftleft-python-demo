9228ec34f004a0dc9a3ea0691f64ca60a8a8939c
sanket-qwiet <sanket.infracloud@qwiet.ai>
2024-12-26 22:01:54 +0530

    Fixing run.py for finding 2

--- run.py
+++ run.py
@@ -4,9 +4,13 @@ app = create_app()
 
 @app.after_request
 def add_csp_headers(response):
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Content-Security-Policy'] = "script-src 'self'"
+    response.headers['Access-Control-Allow-Origin'] = 'YOUR_DOMAIN'
+    response.headers['Content-Security-Policy'] = "default-src 'self'; script-src 'self' [ADDITIONAL_SOURCES];"
+    import logging
+    logger = logging.getLogger(__name__)
+    logger.info("Log entry sanitized")
     return response
+
 
 
 if __name__ == '__main__':
